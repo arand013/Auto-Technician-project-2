68e34da483b47a7a8a365db8cecc3591b19e277f
Alexander Aranda <Alexandermarkets1@gmail.com>
2021-07-27 23:16:42 -0700

    home-route update:

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,34 +1,8 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
-const { Post, User, Comment, Vote } = require('../models');
+//const {} = require('../models');
 
-// get all posts for homepage
-router.get('/', (req, res) => {
-  console.log('======================');
-  Post.findAll({
-    attributes: [
-      'id',
-      'post_url',
-      'title',
-      'created_at',
-      [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
-    ],
-   
-  })
-    .then(dbPostData => {
-      const posts = dbPostData.map(post => post.get({ plain: true }));
-
-      res.render('homepage', {
-        posts,
-        loggedIn: req.session.loggedIn
-      });
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
-    });
-});
-
+// Defines Login 
 router.get('/login', (req, res) => {
   if (req.session.loggedIn) {
     res.redirect('/');
